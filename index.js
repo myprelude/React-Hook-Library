1ce7b11010145f2c20550ea1ab7d5df118ae78d6
何鸣明 <1142929692@qq.com>
2019-08-06 10:31:19 +0800

    Update index.js
    `Add` useInterval useImgLazy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // `export Hooks`
 import useForm form './src/useForm.js'
-
-
+import useInterval form './src/useInterval.js'
+import useImgLazy form './src/useImgLazy.js'
 
 
 export default = {
-	useForm
+	useForm,
+	useInterval,
+	useImgLazy
 }
